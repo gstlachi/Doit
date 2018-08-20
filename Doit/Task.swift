7748180d0dcced7812196781c9a8ae3651c25343
gstlachi <gstlachi@ccc.inaoep.mx>
2018-08-20 10:15:27 -0500

    Clase 164

---
+++ Doit/Task.swift
@@ -0,0 +1,14 @@
+//
+//  task.swift
+//  Doit
+//
+//  Created by German Sanchez Tlachi on 20/08/18.
+//  Copyright © 2018 gstlachiApps. All rights reserved.
+//
+
+import Foundation
+
+class Task  {
+    var name = " "
+    var important = false
+}
